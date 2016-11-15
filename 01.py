2fa5c8870cbb010adc6769ed2f72582831207829
HSLoveZL <hjmrezl@outlook.com>
2016-11-15 13:37:59 +0800

    01,04.py

--- 01.py
+++ 01.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python2.7
 
+lst = [1,2,3,4,5,6]
+L = list(reversed(lst))
+print L
 
---
+++ 04.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# _*_ coding=utf-8 _*_
+# 100 inside can be divided by 3
+L = range(1, 101)
+for x in L:
+    if x % 3 == 0:
+	print x,
+
