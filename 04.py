c9dc8ad1b649ad8f08ca6938992eaf27450a7e57
HSLoveZL <hjmrezl@outlook.com>
2016-11-17 19:44:51 +0800

    04.py

--- 04.py
+++ 04.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # _*_ coding=utf-8 _*_
 # 100 inside can be divided by 3
-L = range(1, 101)
-for x in L:
+L = []
+for x in range(1, 101):
     if x % 3 == 0:
-	print x,
+	L.append(x)
+
+print L
 
